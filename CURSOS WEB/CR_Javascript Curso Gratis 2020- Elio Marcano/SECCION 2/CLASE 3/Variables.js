0195f056ed9c84bbbdd923ee754e296b3328af3a
Alejandro Saul <saul_ba_ve_82@hotmail.com>
2025-06-01 22:21:05 -0600

    Clase 3 y 4 CR_Javascript Curso Gratis 2020- Elio Marcano

---
+++ CURSOS WEB/CR_Javascript Curso Gratis 2020- Elio Marcano/SECCION 2/CLASE 3/Variables.js
@@ -0,0 +1,18 @@
+// var, variables globales
+var saludo = 'Hola CIAPFA'
+if(saludo){
+    var saludo = "HOLA"
+    console.log(saludo)
+}
+console.log(saludo)
+//let, variales liitadas al alcance del bloque donde las definimos
+let nombre = 'CIAPFA'
+if(nombre){
+    let nombre= "HOLA"
+    console.log(nombre)
+}
+console.log(nombre)
+//const, variables inmutables
+const PI = 3.14
+PI = 50
+console.log(PI)
---
+++ CURSOS WEB/CR_Javascript Curso Gratis 2020- Elio Marcano/SECCION 2/CLASE 4/tiposV.js
@@ -0,0 +1,17 @@
+var nombre = 'Alejo' //String
+console.log(nombre)
+var miNumero = 50 //numero
+console.log(miNumero)
+//const PI = 3.14 //decimal
+//console.log(PI)
+var verdadero = true //boolean true
+var falso = false // boolean false
+console.log(verdadero)
+console.log(falso)
+var nulo = null //nula
+console.log(nulo)
+var indefinido = undefined //undefined
+console.log(indefinido)
+console.log(typeof(verdadero)) //imprimir tipo de dato
+const PI = Math.PI
+console.log(PI)
