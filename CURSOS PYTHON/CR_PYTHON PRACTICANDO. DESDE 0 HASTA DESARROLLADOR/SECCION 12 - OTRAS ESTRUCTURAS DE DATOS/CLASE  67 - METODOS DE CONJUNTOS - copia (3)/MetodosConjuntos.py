7d3044a2941469d9a753c5ef01a594de03f12be8
Alejandro Saul <saul_ba_ve_82@hotmail.com>
2025-10-28 04:59:39 -0600

    Python Practicando. Desde 0 hasta Desarrollador en Python - Clase 63 A 69

---
+++ CURSOS PYTHON/CR_PYTHON PRACTICANDO. DESDE 0 HASTA DESARROLLADOR/SECCION 12 - OTRAS ESTRUCTURAS DE DATOS/CLASE  67 - METODOS DE CONJUNTOS - copia (3)/MetodosConjuntos.py
@@ -0,0 +1,20 @@
+conjunto = {1,1,2,3,4,4}
+lista = [1,1,2,3,4,4]
+
+print(conjunto)
+print(lista)
+
+conjunto.add(5)
+print(conjunto)
+
+conjunto.remove(1)
+print(conjunto)
+
+conjunto.pop()
+print(conjunto)
+
+conjunto.update([1,2,3])
+print(conjunto)
+
+conjunto.clear()
+print(conjunto)
---
+++ CURSOS PYTHON/CR_PYTHON PRACTICANDO. DESDE 0 HASTA DESARROLLADOR/SECCION 12 - OTRAS ESTRUCTURAS DE DATOS/CLASE  68 - TUPLAS/Tuplas.py
@@ -0,0 +1,7 @@
+tupla = (1,2,3,4)
+tupla2 = (6,7,8,9)
+print(tupla)
+print(type(tupla))
+print(tupla[0])
+print(tupla[0:3])
+print(tupla+tupla2)
